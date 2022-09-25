1c4c652209fc13165cfc0bf34068f41cf85cc1c7
JesusjrGalvez <jssglvz@gmail.com>
2022-09-25 14:37:48 -0500

    Moved files

--- euler001.py
+++ euler001.py
@@ -1,2 +1,26 @@
 # Problem 001 from Project Euler
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+result = 0
+# Result as a for loop
+
+# for n in range(3, 999):
+#     if n % 3 == 0:
+#         result += n
+#     elif n % 5 == 0:
+#         result += n
+#     else:
+#         pass
+
+
+# Result as a list comprehension
+result = sum(x for x in range(999) if (x % 3 == 0 or x % 5 == 0))
+result = sum(x for x in range(999) if (x % 3 == 0 or x % 5 == 0))
+
+
+print(result)
--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import euler002
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    pass
+    # fib = [1, 2, 3]
+    # print(fib)
+    # for i in range(1000):
+    #     fib.append([-1]+[-2])
+    #     print(fib)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
