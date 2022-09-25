6af6693298082008f4cec4fb2dd15782ef6eb5da
JesusjrGalvez <jssglvz@gmail.com>
2022-09-25 15:42:00 -0500

    Moved files

--- euler003.py
+++ euler003.py
@@ -3,3 +3,37 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 
+primes = []
+
+
+# Function find_prime v.1
+# =======================
+
+def is_prime(n):
+    i = 2
+    for i in range(2, n+1):
+        if n % i == 0 and n != i:
+            return False
+        if i == n:
+            return True
+
+
+# Function find_prime v.2
+# =======================
+
+def is_prime2(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+prime_factors = []
+for i in range(1, 13195):
+    if is_prime2(i) == True:
+        prime_factors.append(i)
+    else:
+        pass
+
+print(prime_factors.pop())
+
+
