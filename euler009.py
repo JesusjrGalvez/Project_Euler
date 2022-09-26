d141de2fad3fc8fc10d6ca00be45a7756570727f
JesusjrGalvez <jssglvz@gmail.com>
2022-09-26 14:45:31 -0500

    Moved files

--- euler009.py
+++ euler009.py
@@ -3,6 +3,12 @@
 
 perimeter = 1000
 
-a + b + c = 1000
-a * a + b * b = c * c
+for a in range(1, 998):
+    for b in range(1, 998):
+        if a + b = 1000:
+        pass
+
+
+# a + b + c = 1000
+# a * a + b * b = c * c
 
