2fe87f9806b50abbe1435356b8214718a914db9a
JesusjrGalvez <jssglvz@gmail.com>
2022-09-25 19:25:46 -0500

    Moved files

--- euler004.py
+++ euler004.py
@@ -3,8 +3,6 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-#from collections import OrderedDict
-
 def is_palindrome(n):
     n_string = str(n)
     n_string_reversed = n_string[::-1]
@@ -19,20 +17,17 @@ for i in range(999, 100, -1):
     for j in range(999, 100, -1):
         k = i * j
         if is_palindrome(k) == True:
-            #palindromes[k] = i, j
-            palindromes[i, j] = k
-            print(f"{k} is a palindrome me of {i} * {j}")
-            #break
-    # print(i)
+            palindromes[k] = i, j
+        else:
+            pass
 
-print(palindromes)
-#print(max(palindromes, key=palindromes.get))
-print(max(palindromes))
+max_product = max(palindromes.keys())
+#print(max_product)
+max_factors = palindromes.get(max_product)
+#print(max_factors)
 
-print(palindromes[max(palindromes)])
-print(palindromes.get(max(palindromes)))
-
-#print(max(palindromes))
+print(f"The largest palindrome made from the product of two 3 digits numbers is {max_product} and \n"
+      f"its factors are {max_factors}")
 
 # Test #2
 # # =======
@@ -44,9 +39,7 @@ print(palindromes.get(max(palindromes)))
 #         print(f"{k} is a palindrome me of {i} * {j}")
 #     #break
 
-#print(is_palindrome(192))
 
-a = {(995, 583): 580085, (995, 517): 514415, (993, 913): 906609, (991, 121): 119911, (987, 286): 282282}
-print(max(a))
+
 
 
--- euler005.py
+++ euler005.py
@@ -1,6 +1,31 @@
-a = {(995, 583): 580085, (995, 517): 514415, (993, 913): 906609, (991, 121): 119911, (987, 286): 282282}
-print(max(a))
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
-print(a)
-print(a[max(a)])
+divisors = [i for i in range(1, 3+1, 1)]
+print(divisors)
+
+
+#n = 1
+
+
+def is_factor(dividend, divisors):
+    for div in divisors:
+        if dividend % div == 0:
+            pass
+        else:
+            return False
+    return True
+
+
+ans = False
+c = 1
+while ans == False:
+    if is_factor(c, divisors) == False:
+        c += 1
+        print(c)
+    else:
+        ans = True
+        break
+        print(f"{c} and {is_factor(c, divisors)}")
