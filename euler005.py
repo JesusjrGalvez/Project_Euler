d3ed9a163a9752e86319943b1cb58e4bee9960da
JesusjrGalvez <jssglvz@gmail.com>
2022-09-25 19:32:44 -0500

    Moved files

--- euler005.py
+++ euler005.py
@@ -3,11 +3,8 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
-divisors = [i for i in range(1, 3+1, 1)]
+divisors = [i for i in range(1, 10+1, 1)]
 print(divisors)
-
-
-#n = 1
 
 
 def is_factor(dividend, divisors):
@@ -21,11 +18,23 @@ def is_factor(dividend, divisors):
 
 ans = False
 c = 1
-while ans == False:
-    if is_factor(c, divisors) == False:
-        c += 1
-        print(c)
+# while ans == False:
+#     if is_factor(c, divisors) == False:
+#         c += 1
+#         print(c)
+#     else:
+#         ans = True
+#         break
+#         print(f"{c} and {is_factor(c, divisors)}")
+
+for i in range(1, 100000000):
+    if is_factor(i, divisors) == False:
+        #print("HI")
+        pass
     else:
-        ans = True
-        break
-        print(f"{c} and {is_factor(c, divisors)}")
+        print(i)
+        #print("hola")
+
+
+
+
